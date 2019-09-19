c709bb6e92445e4a64924b8129fc67ca21a52d85
Sean Yim <superjjang89@gmail.com>
2019-09-19 13:09:23 -0400

    unfinished queries

--- backend-redux-grocery/routes/index.js
+++ backend-redux-grocery/routes/index.js
@@ -1,29 +1,30 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../db');
-
-
 /* GET home page. */
 router.get('/dairy-inv', function(req, res, next) {
- const seedData = [
-    {
-        food: 'milk',
-        quantity: 1
-    },
-    {
-        food: 'cheese',
-        quantity: 1
-    },
-    {
-        food: 'yogurt',
-        quantity: 1
-    },
-    {
-        food: 'kefir',
-        quantity: 2
-    }
-]
-res.json(seedData)
+ const getDiaryQuery = `SELECT * FROM dairy`;
+ db.query(getDiaryQuery, (err,results)=>{
+    if(err){throw err}
+    res.json(results)
+ })
+
 });
+router.post('/update-dept/:dept',(req,res)=>{
+    const dept=req.params.dept.toLowerCase();
+    const newQuantity = req.body.newQuantity;
+    const food = req.body.food;
+    const updateFoodQuery = `
+    UPDATE ?? 
+    SET quantity = ?
+    WHERE food = ? ` //to make it so that hackerz can't see what they're updating
+
+    db.query(updateFoodQuery, [dept, newQuantity, food], (err, results)=>{
+        if(err){throw err}
+    })
+})
+
+
+
 
 module.exports = router;
--- redux-express101/src/actions/AddItemAction.js
+++ redux-express101/src/actions/AddItemAction.js
@@ -1,11 +1,17 @@
+import axios from 'axios';
+
 export default (dept, food, quantity) =>{
     const type = `addItem-${dept}`
+    const updateUrl = `${window.apiHost}/update-dept/${dept}`
+    const updateDept = axios.post(updateUrl,{
+        newQuantity: quantity,
+        food: food
+    })
     return {
         type,
-        payload: {
-            food,
-            quantity
-        }
+        payload: updateDept
+        //we make our request from updateUrl, and the promise is updateDept
+        
 
     }
 }
